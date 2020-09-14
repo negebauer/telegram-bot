1b14f785aa49509198a8ca58311f84bccbcb449c
Nicolas Gebauer <negebauer@hey.com>
2020-09-14 15:03:21 -0300

    dev script

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,21 @@ export function asd(b: String) {
   return b + 'a'
 }
 
+interface User {
+  name: String,
+  age: Number,
+  foo?: Function,
+}
+
+const user: User = {
+  name: a,
+  age: 14
+}
+
+// user.foo = function foo() {
+//   return null
+// }
+
+// user.foo()
+
 console.log(asd(a))
