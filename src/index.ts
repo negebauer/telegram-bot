01792dd17c420cba7b1f52cc1dc0b5f95de7b6b5
Nicolas Gebauer <negebauer@hey.com>
2020-09-14 16:50:04 -0300

    lint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Telegraf } from 'telegraf'
 import config from './config'
 
 const bot = new Telegraf(config.botToken)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('oldschool', ctx => ctx.reply('Hello'))
 bot.command('modern', ({ reply }) => reply('Yo'))
 bot.command('hipster', Telegraf.reply('λ'))
-bot.launch()
+
+bot.launch().then(() => {
+  // eslint-disable-next-line no-console
+  console.log('Bot started successfully')
+})
