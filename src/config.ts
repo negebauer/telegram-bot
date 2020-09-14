b7e193fab0550eb1c7a47e1bab7fbd7402ec2ff5
Nicolas Gebauer <negebauer@hey.com>
2020-09-14 15:34:25 -0300

    example bot

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+const {
+  BOT_TOKEN,
+  NODE_ENV
+} = process.env
+
+const isDev = NODE_ENV === 'development'
+const isProd = NODE_ENV === 'production'
+const isTest = NODE_ENV === 'test'
+
+const config = {
+  env: {
+    isDev,
+    isProd,
+    isTest
+  },
+  botToken: BOT_TOKEN as string
+}
+
+export default config
