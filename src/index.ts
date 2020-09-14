05f304499aef7286f4807c13d88e44e2331a87a0
Nicolas Gebauer <negebauer@hey.com>
2020-09-14 20:11:06 -0300

    session

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,46 @@
-import { Telegraf, Context } from 'telegraf'
+import fs from 'fs'
+import path from 'path'
+import { Telegraf, Context as TelegrafContext } from 'telegraf'
 import LocalSession from 'telegraf-session-local'
 import config from './config'
 
-const bot = new Telegraf(config.botToken)
-const session = new LocalSession({ database: 'db.json' })
+const allowList = fs
+  .readFileSync(path.join(__dirname, 'allowList.txt'), { encoding: 'utf8' })
+  .split('\n')
 
-bot.use(session.middleware())
-
-interface ContextWithSession extends Context {
-  session: {
-    counter: number
-  }
+interface Session {
+  counter?: number
 }
 
-bot.on('text', (ctx1, next) => {
-  const ctx = ctx1 as ContextWithSession
-  ctx.session.counter = ctx.session.counter || 0
-  // eslint-disable-next-line no-plusplus
-  ctx.session.counter++
-  ctx.replyWithMarkdown(
-    `Counter updated, new value: \`${ctx.session.counter}\``,
-  )
+interface BotContext extends TelegrafContext {
+  session: Session
+}
+
+const bot = new Telegraf<BotContext>(config.botToken)
+const session = new LocalSession({ database: config.databaseFileName })
+
+bot.use((ctx, next) => {
+  const { username } = ctx.from || {}
+  const authorized = allowList.includes(username as string)
+  if (!authorized) return ctx.reply('This bot is private')
   return next()
 })
 
-bot.command('oldschool', ctx => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
+bot.use(session.middleware())
+
+bot.use((ctx, next) => {
+  // eslint-disable-next-line camelcase
+  const { id: userId, username, first_name, last_name } = ctx.from || {}
+  const { id: chatId } = ctx.chat || {}
+  Object.assign(ctx.session, {
+    userId,
+    username,
+    first_name,
+    last_name,
+    chatId,
+  })
+  return next()
+})
 
 bot.launch().then(() => {
   // eslint-disable-next-line no-console
