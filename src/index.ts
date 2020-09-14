52069541914ca6ec4a5ee7a6c139eea1396e6082
Nicolas Gebauer <negebauer@hey.com>
2020-09-14 20:58:00 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import fs from 'fs'
 import path from 'path'
-import { Telegraf, Context as TelegrafContext } from 'telegraf'
+import { Telegraf, Context, Stage, BaseScene } from 'telegraf'
 import LocalSession from 'telegraf-session-local'
+import { SceneContextMessageUpdate } from '../node_modules/telegraf/typings/stage.d'
 import config from './config'
 
 const allowList = fs
   .readFileSync(path.join(__dirname, 'allowList.txt'), { encoding: 'utf8' })
   .split('\n')
 
+// types
 interface Session {
   user: {
     username: string
@@ -21,13 +23,19 @@ interface Session {
   }
 }
 
-interface BotContext extends TelegrafContext {
+type i = Context & SceneContextMessageUpdate
+
+interface BotContext extends i {
   session: Session
 }
 
+// init
 const bot = new Telegraf<BotContext>(config.botToken)
-const session = new LocalSession({ database: config.databaseFileName })
+const session = new LocalSession<BotContext>({
+  database: config.databaseFileName,
+})
 
+// middlewares
 bot.use((ctx, next) => {
   const { username } = ctx.from || {}
   const authorized = allowList.includes(username as string)
@@ -51,6 +59,26 @@ bot.use((ctx, next) => {
   return next()
 })
 
+// soccer scene
+const soccerScene = new BaseScene<BotContext>('soccer')
+soccerScene.enter(ctx => {
+  // eslint-disable-next-line no-console
+  console.log(ctx.message)
+})
+soccerScene.leave(ctx => {
+  ctx.reply('bye')
+})
+soccerScene.hears('hi', ctx => ctx.scene.enter('soccer'))
+soccerScene.on('message', ctx => ctx.replyWithMarkdown('Send `hi`'))
+soccerScene.command('back', ctx => ctx.scene.leave())
+
+// bot
+const stage = new Stage<BotContext>([soccerScene], { ttl: 10 })
+bot.use(stage.middleware())
+
+bot.command('soccer', ctx => ctx.scene.enter('soccer'))
+
+// start
 bot.launch().then(() => {
   // eslint-disable-next-line no-console
   console.log('Bot started successfully')
