70c22e6c7e1b83c3ea53a576ea8684c0b089e99f
Nicolas Gebauer <negebauer@hey.com>
2020-09-15 01:17:39 -0300

    loadCommands function

--- scripts/updateCommands.ts
+++ scripts/updateCommands.ts
@@ -1,38 +1,8 @@
-import fs from 'fs'
-import path from 'path'
 import { Telegram } from 'telegraf'
 import config from '../src/config'
+import loadCommands from '../src/loadCommands'
 
-interface Command {
-  command: string
-  description: string
-}
-
-const commandsPath = path.join(__dirname, '..', 'COMMANDS.md')
-const commandsText = fs.readFileSync(commandsPath, { encoding: 'utf8' })
-const commands: Command[] = []
-let nextComand: Command = { command: '', description: '' }
-
-function foundCommand(command: Command) {
-  // eslint-disable-next-line no-param-reassign
-  command.description = command.description.trim()
-  commands.push(command)
-}
-
-commandsText.split('\n').forEach(line => {
-  if (line.substring(0, 2) === '##') {
-    if (nextComand.command) foundCommand(nextComand)
-
-    nextComand = {
-      command: line.replace('## ', ''),
-      description: '',
-    }
-  } else {
-    nextComand.description += ` ${line}`
-  }
-})
-foundCommand(nextComand)
-
+const commands = loadCommands()
 const telegram = new Telegram(config.botToken)
 telegram.setMyCommands(commands)
 // eslint-disable-next-line no-console
